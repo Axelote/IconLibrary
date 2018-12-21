04f1cbd504e9705bf8165b5ca76977577c5b44ce
Ivan Chepiha <chepiga@rozetka.com.ua>
2018-12-21 18:24:57 +0200

    added ng app and upload function

---
+++ ng-i-short/src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { environment } from '../environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  items: Observable<any[]>;
+  uploadPercent: Observable<number>;
+  downloadURL: string;
+  _db:AngularFirestore;
+  imageSrc = 'https://firebasestorage.googleapis.com/v0/b/'+environment.firebaseConfig.storageBucket+'/o/icons%2F';
+
+  constructor(db: AngularFirestore, private storage: AngularFireStorage) {
+    this.items = db.collection('icons').valueChanges();
+    this._db = db;
+  }
+
+  uploadFile(event: { target: { files: any[]; }; }) {
+    const file = event.target.files[0];
+    const filePath = file.name;
+    const fileRef = this.storage.ref('icons/').child(filePath);
+    const task = this.storage.upload(filePath, file);
+    const iconsCollection = this._db.collection('icons');
+    iconsCollection.add({url: file.name});
+    
+    // observe percentage changes
+    this.uploadPercent = task.percentageChanges();
+    // get notified when the download URL is available
+    // task.snapshotChanges().pipe(
+    //     finalize( () => this.downloadURL = this.success )
+    // )
+    // .subscribe()
+  }
+}
---
+++ ng-i-short/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyA4bPzjvJYbFPd8rUHuyRLDQJYg-KHzAM0",
+    authDomain: "icon-library-b4868.firebaseapp.com",
+    databaseURL: "https://icon-library-b4868.firebaseio.com",
+    projectId: "icon-library-b4868",
+    storageBucket: "icon-library-b4868.appspot.com",
+    messagingSenderId: "446617708514"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
